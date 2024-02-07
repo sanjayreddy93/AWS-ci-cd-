e0eb03a4ad52bc2db6e5f69addb8a037fc24d6a3
Sanjaykumar Beerelli <sanjaykumar.beerelli@devoteam.com>
2024-02-07 09:16:07 +0100

    initial commit

---
+++ providers.tf
@@ -0,0 +1,17 @@
+terraform {
+    backend "s3" {
+        bucket = "tfstate-backend-2044"
+        key = "state_files/terraform.tfstate"
+        region = "eu-central-1"
+    }
+  required_providers {
+    aws = {
+        version = "5.33.0"
+        source = "hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-central-1"
+}
