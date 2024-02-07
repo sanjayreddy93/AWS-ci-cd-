d4f754fb66afc2d2c9e295f96692225b43c50000
sanjayreddy93 <sanjaybeerelli@gmail.com>
2024-02-07 12:02:28 +0100

    Updated controller return

--- code/src/main/java/com/example/demo/HelloController.java
+++ code/src/main/java/com/example/demo/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @RequestMapping
     public String index(){
-        return "Hello From Spring boot!!";
+        return "Hi, From Spring boot!!";
     }
 }
