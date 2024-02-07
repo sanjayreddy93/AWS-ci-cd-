c2cc0fc877d260f1d922ae39cc7842226ce0adce
sanjayreddy93 <sanjaybeerelli@gmail.com>
2024-02-07 11:23:24 +0100

    Added github workflows

--- providers.tf
+++ providers.tf
@@ -1,13 +1,13 @@
 terraform {
-    backend "s3" {
-        bucket = "tfstate-backend-2044"
-        key = "state_files/terraform.tfstate"
-        region = "eu-central-1"
-    }
+  backend "s3" {
+    bucket = "tfstate-backend-2044"
+    key    = "state_files/terraform.tfstate"
+    region = "eu-central-1"
+  }
   required_providers {
     aws = {
-        version = "5.33.0"
-        source = "hashicorp/aws"
+      version = "5.33.0"
+      source  = "hashicorp/aws"
     }
   }
 }
